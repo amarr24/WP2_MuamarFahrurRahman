96b1e5b7321dc8e534d65a81e5b4b45bcd2be55a
amarr24 <muamarfahrurrahman2004@gmail.com>
2023-09-25 16:09:53 +0700

    menambahkan folder booking-pustaka

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama: Muamar Fahrur Rahman
+             Nim: 19220849
+             kelas: 19.3A.26
+            Saya tinggal di daerah Kebon Jeruk
+            olahraga yang saya sukai adalah
+            Lari";
+    }
+}
