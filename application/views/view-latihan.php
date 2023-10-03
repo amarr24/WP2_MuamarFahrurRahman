227c62d5513c368101018d52ec56e7bfd7bd6fda
amarr24 <muamarfahrurrahman2004@gmail.com>
2023-10-03 20:54:57 +0700

    menambahkan File Pertemuan 3

--- application/views/view-latihan.php
+++ application/views/view-latihan.php
@@ -1,15 +1,13 @@
 <html>
 <head>
-    <title>Latihan 1</title>
+    <title>latihan1</title>
 </head>
 
 <body>
     Halo Kawan.. Yuk kita belajar web programming..!!!<br>
     Nilai 1 = <?= $nilai1; ?>
-    Nilai 2 = <?= $nilai2; ?>
+    Nilai 2 = <?= $nilai2; ?> <br>
     ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
-$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
-
+    $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
 </body>
-
 </html>
